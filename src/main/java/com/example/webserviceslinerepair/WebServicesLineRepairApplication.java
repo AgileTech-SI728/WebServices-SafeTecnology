95fcb8c4cbcf2ec16b53e154debf6f1c3da42722
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-05-30 20:54:22 -0500

    add initial files

---
+++ src/main/java/com/example/webserviceslinerepair/WebServicesLineRepairApplication.java
@@ -0,0 +1,13 @@
+package com.example.webserviceslinerepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesLineRepairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicesLineRepairApplication.class, args);
+    }
+
+}
