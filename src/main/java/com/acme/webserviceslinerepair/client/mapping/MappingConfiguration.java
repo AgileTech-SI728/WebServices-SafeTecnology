5a5f4be256f1e1482ddc9c99a8845705c3c577de
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-03 13:57:12 -0500

    add client mapping

--- src/main/java/com/acme/webserviceslinerepair/client/mapping/MappingConfiguration.java
+++ src/main/java/com/acme/webserviceslinerepair/client/mapping/MappingConfiguration.java
@@ -1,4 +1,12 @@
 package com.acme.webserviceslinerepair.client.mapping;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("clientMappingConfiguration")
 public class MappingConfiguration {
+    @Bean
+    public ClientMapper clientMapper(){
+        return new ClientMapper();
+    }
 }
