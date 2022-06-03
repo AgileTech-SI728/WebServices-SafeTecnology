df158b4ea84c62b400e1d46a0fe0cf09443e029e
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-03 13:11:16 -0500

    add appointment mapping

--- src/main/java/com/acme/webserviceslinerepair/appointment/mapping/AppointmentMapper.java
+++ src/main/java/com/acme/webserviceslinerepair/appointment/mapping/AppointmentMapper.java
@@ -6,6 +6,9 @@ import com.acme.webserviceslinerepair.appointment.resource.CreateAppointmentReso
 import com.acme.webserviceslinerepair.appointment.resource.UpdateAppointmentResource;
 import com.acme.webserviceslinerepair.shared.mapping.EnhancedModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.io.Serializable;
 import java.util.List;
@@ -29,5 +32,9 @@ public class AppointmentMapper implements Serializable {
 
     public Appointment toModel(UpdateAppointmentResource resource){
         return mapper.map(resource, Appointment.class);
+    }
+
+    public Page<AppointmentResource> modelListPage(List<Appointment> modelList, Pageable pageable){
+        return new PageImpl<>(mapper.mapList(modelList, AppointmentResource.class), pageable, modelList.size());
     }
 }
