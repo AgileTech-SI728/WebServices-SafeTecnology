9bcdad4fbcaa701a023bfdda1b45934007e82fc9
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-03 13:57:02 -0500

    add client domain

--- src/main/java/com/acme/webserviceslinerepair/client/domain/persistence/ClientRepository.java
+++ src/main/java/com/acme/webserviceslinerepair/client/domain/persistence/ClientRepository.java
@@ -1,4 +1,14 @@
 package com.acme.webserviceslinerepair.client.domain.persistence;
 
-public interface ClientRepository {
-}
+import com.acme.webserviceslinerepair.client.domain.model.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    Client findByEmail(String email);
+        List<Client> findByNameAndLastNameContaining(String names, String lastNames);
+
+        }
