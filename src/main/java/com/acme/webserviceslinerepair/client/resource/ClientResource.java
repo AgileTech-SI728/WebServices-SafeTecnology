b88b1115c2115bb7b586bad1ed311331b13eca03
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-03 13:57:23 -0500

    add client resource

--- src/main/java/com/acme/webserviceslinerepair/client/resource/ClientResource.java
+++ src/main/java/com/acme/webserviceslinerepair/client/resource/ClientResource.java
@@ -1,4 +1,17 @@
 package com.acme.webserviceslinerepair.client.resource;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ClientResource {
+    private Long id;
+    private String names;
+    private String lastNames;
+    private String address;
+    private String email;
+    private String password;
+    private String cellPhoneNumber;
+    private String planType;
 }
