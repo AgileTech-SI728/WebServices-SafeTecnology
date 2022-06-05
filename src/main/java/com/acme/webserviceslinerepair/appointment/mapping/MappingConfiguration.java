85c3af16cfa81f11f42a32801a6eec07d7964235
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-04 19:15:07 -0500

    add appointment mapping

--- src/main/java/com/acme/webserviceslinerepair/appointment/mapping/MappingConfiguration.java
+++ src/main/java/com/acme/webserviceslinerepair/appointment/mapping/MappingConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration("appointmentMappingConfiguration")
 public class MappingConfiguration {
-
     @Bean
     public AppointmentMapper appointmentMapper(){
         return new AppointmentMapper();
