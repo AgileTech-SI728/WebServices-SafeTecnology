4c529c4942c3ca5b8359f5d07f04317c1c697dd0
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-04 20:16:19 -0500

    add technician domain

--- src/main/java/com/acme/webserviceslinerepair/technician/domain/persistence/TechnicianRepository.java
+++ src/main/java/com/acme/webserviceslinerepair/technician/domain/persistence/TechnicianRepository.java
@@ -1,4 +1,14 @@
 package com.acme.webserviceslinerepair.technician.domain.persistence;
 
-public interface TechnicianRepository {
+import com.acme.webserviceslinerepair.client.domain.model.entity.Client;
+import com.acme.webserviceslinerepair.technician.domain.model.entity.Technician;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TechnicianRepository extends JpaRepository<Technician, Long> {
+
+    Technician findByEmail(String email);
+    List<Technician> findByNameAndLastNameContaining(String names, String lastNames);
+
 }
--- src/main/java/com/acme/webserviceslinerepair/technician/domain/service/TechnicianService.java
+++ src/main/java/com/acme/webserviceslinerepair/technician/domain/service/TechnicianService.java
@@ -1,4 +1,17 @@
 package com.acme.webserviceslinerepair.technician.domain.service;
 
+import com.acme.webserviceslinerepair.client.domain.model.entity.Client;
+import com.acme.webserviceslinerepair.technician.domain.model.entity.Technician;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface TechnicianService {
+    List<Technician> getAll();
+    Technician getById(Long technicianId);
+    Technician getByEmail(String email);
+    List<Technician> getByNameAndLastName(String names, String lastNames);
+    Technician create(Technician technician);
+    Technician update(Long technicianId, Technician technician);
+    ResponseEntity<?> delete(Long technicianId);
 }
