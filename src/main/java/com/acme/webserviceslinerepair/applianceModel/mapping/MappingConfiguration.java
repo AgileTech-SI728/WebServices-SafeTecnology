a864287ecfebd13af51e5afe734162a88f05d206
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-04 19:15:36 -0500

    add applianceModel mapping

--- src/main/java/com/acme/webserviceslinerepair/applianceModel/mapping/MappingConfiguration.java
+++ src/main/java/com/acme/webserviceslinerepair/applianceModel/mapping/MappingConfiguration.java
@@ -1,4 +1,12 @@
 package com.acme.webserviceslinerepair.applianceModel.mapping;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("applianceModelMappingConfiguration")
 public class MappingConfiguration {
+    @Bean
+    public ApplianceModelMapper applianceModelMapper(){
+        return new ApplianceModelMapper();
+    }
 }
