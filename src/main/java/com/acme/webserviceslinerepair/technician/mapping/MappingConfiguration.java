0a428da166137b81b4ec66de83e9be731957f103
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-04 20:16:28 -0500

    add technician mapping

--- src/main/java/com/acme/webserviceslinerepair/technician/mapping/MappingConfiguration.java
+++ src/main/java/com/acme/webserviceslinerepair/technician/mapping/MappingConfiguration.java
@@ -1,4 +1,12 @@
 package com.acme.webserviceslinerepair.technician.mapping;
 
+import com.acme.webserviceslinerepair.client.mapping.ClientMapper;
+import org.springframework.context.annotation.Bean;
+
 public class MappingConfiguration {
+    @Bean
+    public TechnicianMapper technicianMapper(){
+        return new TechnicianMapper();
+    }
 }
+
