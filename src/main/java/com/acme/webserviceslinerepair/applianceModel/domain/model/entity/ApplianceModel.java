398fe4b87ab1dc99e6edd99cd30abb14f889409f
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-04 19:15:23 -0500

    add applianceModel domain

--- src/main/java/com/acme/webserviceslinerepair/applianceModel/domain/model/entity/ApplianceModel.java
+++ src/main/java/com/acme/webserviceslinerepair/applianceModel/domain/model/entity/ApplianceModel.java
@@ -1,4 +1,44 @@
 package com.acme.webserviceslinerepair.applianceModel.domain.model.entity;
 
-public class ApplianceModel {
+import com.acme.webserviceslinerepair.client.domain.model.entity.Client;
+import com.acme.webserviceslinerepair.shared.domain.model.AuditModel;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@With
+@Entity
+@Table(name = "applianceModel")
+public class ApplianceModel extends AuditModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max=300)
+    private String name;
+
+    @NotNull
+    @NotBlank
+    @Size(max=300)
+    private String model;
+
+    @NotNull
+    @NotBlank
+    @Size(max=50)
+    private String imagePath;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "client_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Client client;
 }
