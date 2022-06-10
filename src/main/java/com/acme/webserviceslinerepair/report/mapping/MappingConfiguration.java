69da841a1ae0f4f1db961a32f3d4999b30dcc2b5
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-10 12:12:59 -0500

    add report mapping

--- src/main/java/com/acme/webserviceslinerepair/report/mapping/MappingConfiguration.java
+++ src/main/java/com/acme/webserviceslinerepair/report/mapping/MappingConfiguration.java
@@ -1,4 +1,13 @@
 package com.acme.webserviceslinerepair.report.mapping;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("reportMappingConfiguration")
 public class MappingConfiguration {
+
+    @Bean
+    public ReportMapper reportMapper(){
+        return new ReportMapper();
+    }
 }
