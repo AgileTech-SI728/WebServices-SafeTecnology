202b7d544eece315ae317893d576a63b7f9b140a
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-11 21:27:51 -0500

    update appointment controller

--- src/main/java/com/acme/webserviceslinerepair/appointment/api/AppointmentsController.java
+++ src/main/java/com/acme/webserviceslinerepair/appointment/api/AppointmentsController.java
@@ -1,12 +1,15 @@
 package com.acme.webserviceslinerepair.appointment.api;
 
-import com.acme.webserviceslinerepair.applianceModel.resource.ApplianceModelResource;
 import com.acme.webserviceslinerepair.appointment.domain.service.AppointmentService;
 import com.acme.webserviceslinerepair.appointment.mapping.AppointmentMapper;
 import com.acme.webserviceslinerepair.appointment.resource.AppointmentResource;
 import com.acme.webserviceslinerepair.appointment.resource.CreateAppointmentResource;
 import com.acme.webserviceslinerepair.appointment.resource.UpdateAppointmentResource;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,7 +21,7 @@ import java.util.List;
 
 @Tag(name = "Appointment")
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/appointments")
 @CrossOrigin
 public class AppointmentsController {
     @Autowired
@@ -31,34 +34,35 @@ public class AppointmentsController {
         this.mapper = mapper;
     }
     @Operation(summary = "Get All Appointments", description = "Get All Appointments")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Appointments found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AppointmentResource.class))})
+    })
     @GetMapping("appointments")
     public List<AppointmentResource> getAll(){
         return mapper.toResource(appointmentService.getAll());
     }
 
     @Operation(summary = "Get Appointment by Id", description = "Get Appointment by Id")
-    @GetMapping("appointments/{appointmentId}")
+    @GetMapping("{appointmentId}")
     public AppointmentResource getAppointmentById(@PathVariable Long appointmentId){
         return mapper.toResource(appointmentService.getById(appointmentId));
     }
-    @GetMapping
-    public Page<AppointmentResource> getAllRents(Pageable pageable) {
-        return mapper.modelListToPage(appointmentService.getAll(), pageable);
-    }
     @Operation(summary = "Get Appointments by ClientId", description = "Get All Appointments by ClientId")
-    @GetMapping("clients/{clientId}/appointments")
+    @GetMapping("{clientId}/appointments")
     public List<AppointmentResource> getAppointmentsByClientId(@PathVariable Long clientId){
         return mapper.toResource(appointmentService.getByClientId(clientId));
     }
 
     @Operation(summary = "Create New Appointment", description = "Create New Appointment")
-    @PostMapping("clients/{clientId}/applianceModel/{applianceModelId}/appointments")
+    @PostMapping("{clientId}/{applianceModelId}/appointments")
     public AppointmentResource createAppointment(@RequestBody CreateAppointmentResource model, @PathVariable Long clientId, @PathVariable Long applianceModelId){
         return mapper.toResource(appointmentService.create(mapper.toModel(model), clientId, applianceModelId));
     }
 
     @Operation(summary = "Update Appointment", description = "Update Appointment")
-    @PutMapping("appointments/{appointmentId}")
+    @PutMapping("{appointmentId}")
     public AppointmentResource updateAppointment(@PathVariable Long appointmentId, @RequestBody UpdateAppointmentResource model){
         return mapper.toResource(appointmentService.update(appointmentId, mapper.toModel(model)));
     }
