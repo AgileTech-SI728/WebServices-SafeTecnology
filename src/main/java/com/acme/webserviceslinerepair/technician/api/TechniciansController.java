685c92f46612693c603da2a2432ac3c9a2863b5e
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-11 21:26:30 -0500

    update technician controller

--- src/main/java/com/acme/webserviceslinerepair/technician/api/TechniciansController.java
+++ src/main/java/com/acme/webserviceslinerepair/technician/api/TechniciansController.java
@@ -6,6 +6,10 @@ import com.acme.webserviceslinerepair.technician.resource.CreateTechnicianResour
 import com.acme.webserviceslinerepair.technician.resource.TechnicianResource;
 import com.acme.webserviceslinerepair.technician.resource.UpdateTechnicianResource;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+@Tag(name = "Technician")
 @RestController
 @RequestMapping("api/v1/technicians")
 @CrossOrigin
@@ -29,36 +34,37 @@ public class TechniciansController {
     }
 
     @Operation(summary = "Get All Technicians", description = "Get All Technicians")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Technicians found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TechnicianResource.class))})
+    })
     @GetMapping("technicians")
     public List<TechnicianResource> getAll(){
         return mapper.toResource(technicianService.getAll());
     }
     @Operation(summary = "Get Technician by Email", description = "Get Technician by Email")
-    @GetMapping("emails/{email}")
+    @GetMapping("{email}")
     public TechnicianResource getTechnicianByEmail(@PathVariable String email){
         return mapper.toResource(technicianService.getByEmail(email));
     }
-    @GetMapping
-    public Page<TechnicianResource> getAllRents(Pageable pageable) {
-        return mapper.modelListToPage(technicianService.getAll(), pageable);
-    }
     @Operation(summary = "Get Technician by Complete Name", description = "Get Technician by Complete Name")
-    @GetMapping("names/{names}/lastNames/{lastNames}")
+    @GetMapping("{names}/{lastNames}")
     public List<TechnicianResource> getTechnicianByNameAndLastName(@PathVariable String names, @PathVariable String lastNames){
         return mapper.toResource(technicianService.getByNameAndLastName(names, lastNames));
     }
     @Operation(summary = "Get Technician by Id", description = "Get Technician by Id")
-    @GetMapping("technicians/{technicianId}")
+    @GetMapping("{technicianId}")
     public TechnicianResource getTechnicianById(@PathVariable Long technicianId){
         return mapper.toResource(technicianService.getById(technicianId));
     }
     @Operation(summary = "Create New Technician", description = "Create New Technician")
-    @PostMapping("technicians")
+    @PostMapping
     public TechnicianResource createTechnician(@RequestBody CreateTechnicianResource resource){
         return mapper.toResource(technicianService.create(mapper.toModel(resource)));
     }
     @Operation(summary = "Update Technician", description = "Update Technician")
-    @PutMapping("technicians/{technicianId}")
+    @PutMapping("{technicianId}")
     public TechnicianResource updateTechnician(@PathVariable Long technicianId, @RequestBody UpdateTechnicianResource model){
         return mapper.toResource(technicianService.update(technicianId, mapper.toModel(model)));
     }
