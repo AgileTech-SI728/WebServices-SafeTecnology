a91ff72677d16a096cef43e4ea590261086f2dc1
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-11 21:26:10 -0500

    update report controller

--- src/main/java/com/acme/webserviceslinerepair/report/api/ReportsController.java
+++ src/main/java/com/acme/webserviceslinerepair/report/api/ReportsController.java
@@ -1,13 +1,16 @@
 package com.acme.webserviceslinerepair.report.api;
 
-import com.acme.webserviceslinerepair.applianceModel.resource.ApplianceModelResource;
-import com.acme.webserviceslinerepair.applianceModel.resource.CreateApplianceModelResource;
 import com.acme.webserviceslinerepair.report.domain.service.ReportService;
 import com.acme.webserviceslinerepair.report.mapping.ReportMapper;
 import com.acme.webserviceslinerepair.report.resource.CreateReportResource;
 import com.acme.webserviceslinerepair.report.resource.ReportResource;
 import com.acme.webserviceslinerepair.report.resource.UpdateReportResource;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+@Tag(name = "Report")
 @RestController
 @RequestMapping("api/v1/reports")
 @CrossOrigin
@@ -31,26 +35,27 @@ public class ReportsController {
 
 
     @Operation(summary = "Get All Reports", description = "Get All Reports")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Reports found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ReportResource.class))})
+    })
     @GetMapping("reports")
     public List<ReportResource> getAll(){
         return mapper.toResource(reportService.getAll());
     }
-    @GetMapping
-    public Page<ReportResource> getAllRents(Pageable pageable) {
-        return mapper.modelListToPage(reportService.getAll(), pageable);
-    }
     @Operation(summary = "Get Report by Id", description = "Get Report by Id")
-    @GetMapping("reports/{reportId}")
+    @GetMapping("{reportId}")
     public ReportResource getReportById(@PathVariable Long reportId){
         return mapper.toResource(reportService.getById(reportId));
     }
     @Operation(summary = "Create New Report", description = "Create New Report")
-    @PostMapping("technicians/{technicianId}/reports")
+    @PostMapping("{technicianId}/reports")
     public ReportResource createReport(@RequestBody CreateReportResource resource, @PathVariable Long technicianId){
         return mapper.toResource(reportService.create(mapper.toModel(resource), technicianId));
     }
     @Operation(summary = "Update Report", description = "Update Report")
-    @PutMapping("reports/{reportId}")
+    @PutMapping("{reportId}")
     public ReportResource updateReport(@PathVariable Long reportId, @RequestBody UpdateReportResource model){
         return mapper.toResource(reportService.update(reportId, mapper.toModel(model)));
     }
