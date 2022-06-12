ec3f380e96627f4ca12e74162e2aea3ee25d4ab4
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-11 21:27:02 -0500

    update report domain

--- src/main/java/com/acme/webserviceslinerepair/report/domain/service/ReportService.java
+++ src/main/java/com/acme/webserviceslinerepair/report/domain/service/ReportService.java
@@ -1,6 +1,8 @@
 package com.acme.webserviceslinerepair.report.domain.service;
 
 import com.acme.webserviceslinerepair.report.domain.model.entity.Report;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.List;
 public interface ReportService {
     List<Report> getAll();
     Report getById(Long reportId);
+    Page<Report> getAll(Pageable pageable);
     List<Report> getByTechnicianId(Long technicianId);
     Report create(Report report, Long technicianId);
     Report update(Long reportId, Report report);
