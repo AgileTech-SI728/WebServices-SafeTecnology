9d0f2371906e86dd67110dd7bd185a6eff39436f
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-24 12:28:30 -0500

    Update WebServicesLinerepairApplication.java

--- src/main/java/com/acme/webserviceslinerepair/WebServicesLinerepairApplication.java
+++ src/main/java/com/acme/webserviceslinerepair/WebServicesLinerepairApplication.java
@@ -1,6 +1,9 @@
 package com.acme.webserviceslinerepair;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@SecurityScheme(name="acme", scheme = "bearer",
+		type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 @OpenAPIDefinition
 @EnableJpaAuditing
 @SpringBootApplication
